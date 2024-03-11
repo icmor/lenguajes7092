a931ea624a23c787a947f8386426f3e15d4e285c
icmor <cornejodlm@ciencias.unam.mx>
2024-03-11 13:29:37 -0600

    Ejercicio 2

--- practica02/practica2.rkt
+++ practica02/practica2.rkt
@@ -6,3 +6,16 @@ Practica 2.
 ;; Equipo
 ;; Iñaki Cornejo de la Mora
 ;; Dahlia Yali Osorio Rangel
+;; Ejercicio 2
+(define err-index-too-big "El índice es mayor o igual que el tamaño de la lista")
+(define err-index-too-small "El índice no puede ser menor que 0")
+
+;; Función para obtener el elemento de una lista a partir de su índice
+(define (get-by-index index ls)
+  (define (get-elem idx ls)
+    (if (zero? idx) (car ls)
+        (get-elem (sub1 idx) (cdr ls))))
+  (cond ((< index 0) error 'get-by-index err-index-too-small)
+        ((>= index (length ls)) error 'get-by-index err-index-too-big)
+        (else (get-elem index ls))))
+
